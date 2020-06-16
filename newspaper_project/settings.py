5f7710148d95413664c70b7a0c2c23aad0df7aad
Estefano Felipa <estefano_fc@hotmail.com>
2020-06-15 17:27:10 -0700

    added tests

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "users.apps.UsersConfig",
+    "pages.apps.PagesConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -118,4 +119,3 @@ AUTH_USER_MODEL = "users.CustomUser"
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
-
